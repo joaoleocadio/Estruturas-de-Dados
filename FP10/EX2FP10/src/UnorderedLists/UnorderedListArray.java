97b0567e1809167d5252014a95af8dd26e459e1b
joaoleocadio <joaocarvalho388@gmail.com>
2021-12-17 15:59:35 +0000

    bug fix in unorderedList

--- FP10/EX2FP10/src/UnorderedLists/UnorderedListArray.java
+++ FP10/EX2FP10/src/UnorderedLists/UnorderedListArray.java
@@ -4,7 +4,6 @@ import Exceptions.ListsException;
 import Interfaces.ArrayList;
 import Interfaces.UnorderedListADT;
 
-
 /**
  *
  * @author joaoc
@@ -13,7 +12,6 @@ public class UnorderedListArray<T> extends ArrayList<T> implements UnorderedList
 
     public UnorderedListArray() {
     }
-    
 
     @Override
     public void addToFront(T element) throws ListsException {
@@ -25,7 +23,7 @@ public class UnorderedListArray<T> extends ArrayList<T> implements UnorderedList
             for (int i = size() - 1; i >= 0; i--) {
                 list[i + 1] = list[i];
             }
-            
+
             list[0] = element;
         }
         cont++;
@@ -36,56 +34,60 @@ public class UnorderedListArray<T> extends ArrayList<T> implements UnorderedList
     public void addToRear(T element) throws ListsException {
         if (isEmpty()) {
             list[0] = element;
-        } else if (size() == list.length) {
-            expandList();
         } else {
-            list[size()] = element;
+            if (cont == list.length) {
+                expandList();
+            }
+            
+            list[cont] = element;        
         }
-        
         cont++;
         modCount++;
-    }
+        }
 
-    @Override
-    public void addAfter(T element, T target) throws ListsException {
-        if (isEmpty()) {
-            throw new ListsException(ListsException.EMPTYCOLLECTION);
-        } else if (size() == list.length) {
-            expandList();
-        } else {
-            
-            boolean found = false;
-            int pos = 0;
-            
-            //Percorrer o array
-            for (int i = 0; i < size(); i++) {
-                if (list[pos].equals(target)) {
-                    found = true;
-                } else {
-                    pos++;
-                }
-            }
-            
-            if (found == true) {
-                for (int i = size() - 1; i > pos; i--) {
-                    list[i + 1] = list[i];
-                }
-                list[pos + 1] = element;
-                cont++;
-                modCount++;
+        @Override
+        public void addAfter
+        (T element, T target) throws ListsException {
+            if (isEmpty()) {
+                throw new ListsException(ListsException.EMPTYCOLLECTION);
+            } else if (size() == list.length) {
+                expandList();
             } else {
-                throw new ListsException("Element not Found!");
+
+                boolean found = false;
+                int pos = 0;
+
+                //Percorrer o array
+                for (int i = 0; i < size(); i++) {
+                    if (list[pos].equals(target)) {
+                        found = true;
+                    } else {
+                        pos++;
+                    }
+                }
+
+                if (found == true) {
+                    for (int i = size() - 1; i > pos; i--) {
+                        list[i + 1] = list[i];
+                    }
+                    list[pos + 1] = element;
+                    cont++;
+                    modCount++;
+                } else {
+                    throw new ListsException("Element not Found!");
+                }
             }
         }
-    }
     
+    
+
     private void expandList() {
         T[] tmp = (T[]) new Object[list.length + DEFAULT_CAPACITY];
-        
+
         for (int i = 0; i < size(); i++) {
             tmp[i] = list[i];
         }
-        
+
         list = tmp;
-    } 
+    }
 }
